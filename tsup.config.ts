a9324850f129e166e43efe26deb713208a468221
Jérôme <jfreyheit@gmail.com>
2025-01-09 12:23:19 +0700

    fix: fix compilation, stop hard-coding files list

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,26 +1,21 @@
+import { globSync } from "fs";
 import { defineConfig } from "tsup";
+
+const entry = globSync("./src/**/index.ts").filter(
+  (name) => !name.includes("src/test/setup"),
+);
 
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: [
-    "cookie",
-    "http/headers",
-    "http/search_params",
-    "http/status",
-    "jwt",
-    "kysely",
-    "oauth_providers",
-    "test/matchers",
-    "validation",
-  ].map((folder) => `./src/${folder}/index.ts`),
+  entry,
   format: "esm",
   outExtension() {
     return {
       js: `.mjs`,
     };
   },
-  sourcemap: true,
+  sourcemap: false,
   splitting: false,
   target: "node22",
 });
