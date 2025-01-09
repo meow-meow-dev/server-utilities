c6030d12e5425cb1db11636903ed36aa14cfb756
Jérôme <jfreyheit@gmail.com>
2025-01-10 00:18:25 +0700

    feat: add helpers for durations & timestamps

--- src/jwt/JWTPayloadTimestamps.ts
+++ src/jwt/JWTPayloadTimestamps.ts
@@ -1,6 +1,8 @@
 import { integerSchema } from "@meow-meow-dev/server-utilities/~validation";
 import * as v from "valibot";
 
+import { type Duration, durationToSeconds } from "./duration.js";
+
 export const jwtPayloadTimestampsSchema = v.strictObject({
   exp: integerSchema,
   iat: integerSchema,
@@ -11,15 +13,33 @@ export type JWTPayloadTimestamps = v.InferOutput<
   typeof jwtPayloadTimestampsSchema
 >;
 
-type BuildJWTPayloadTimestamps = {
-  expiryInSeconds: number;
+export type WithJWTTimeStamps<P extends Record<string, unknown>> =
+  JWTPayloadTimestamps & P;
+
+export type WithoutJWTTimeStamps<P extends JWTPayloadTimestamps> = Omit<
+  P,
+  keyof JWTPayloadTimestamps
+>;
+
+type BuildJWTPayloadTimestampsProps = {
+  expiry: Duration;
 };
 
+export function addJWTPayloadTimestamps<P extends Record<string, unknown>>(
+  payload: P,
+  options: BuildJWTPayloadTimestampsProps,
+): JWTPayloadTimestamps & P {
+  return {
+    ...payload,
+    ...buildJWTPayloadTimestamps(options),
+  };
+}
+
 export function buildJWTPayloadTimestamps({
-  expiryInSeconds,
-}: BuildJWTPayloadTimestamps): JWTPayloadTimestamps {
+  expiry,
+}: BuildJWTPayloadTimestampsProps): JWTPayloadTimestamps {
   const iat = Math.floor(Date.now() / 1000); // number of seconds since epoch
-  const exp = iat + expiryInSeconds;
+  const exp = iat + durationToSeconds(expiry);
 
   return {
     exp,
