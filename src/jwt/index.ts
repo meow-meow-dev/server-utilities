4dd100e96291de5efa504e0c24ceabcc102a09d9
jfreyheit <jfreyheit@gmail.com>
2025-01-11 01:58:34 +0700

    fix: stop using import.meta.env to determine http-only cookies options (#48)

--- src/jwt/index.ts
+++ src/jwt/index.ts
@@ -2,5 +2,4 @@ export * from "./accessTokenPayload.js";
 export * from "./duration.js";
 export * from "./extractJWTPayload.js";
 export * from "./JWTPayloadTimestamps.js";
-export * from "./setHTTPOnlyCookie.js";
 export * from "./standardIdTokenClaimsSchemas.js";
