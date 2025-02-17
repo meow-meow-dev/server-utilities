72917d2d0cf06ff52892bae121a09599c3666c1f
Jérôme <jfreyheit@gmail.com>
2025-02-17 23:46:03 +0530

    fix: fix unit tests + lint errors

--- src/http/headers/mergeHeaders.ts
+++ src/http/headers/mergeHeaders.ts
@@ -2,14 +2,12 @@ export function mergeHeaders(
   currentHeader: HeadersInit,
   addedFields: Record<string, string>,
 ): HeadersInit {
-  if (currentHeader instanceof Headers) {
-    for (const [keyboard, value] of Object.entries(addedFields))
-      currentHeader.set(keyboard, value);
-
-    return currentHeader;
-  } else if (Array.isArray(currentHeader)) {
+  if (Array.isArray(currentHeader)) {
     return [...currentHeader, ...Object.entries(addedFields)];
   } else {
-    return { ...currentHeader, ...addedFields };
+    const newHeader = new Headers(currentHeader);
+    for (const [key, value] of Object.entries(addedFields))
+      newHeader.set(key, value);
+    return newHeader;
   }
 }
