9045eac010b1eb304277061af89cf32676f52b4e
jfreyheit <jfreyheit@gmail.com>
2025-03-11 20:48:51 +0700

    feat: export extra files in queries/, add kysely debug options (#78)

--- src/queries/createQuery.ts
+++ src/queries/createQuery.ts
@@ -1,5 +1,6 @@
 import { errAsync, okAsync } from "neverthrow";
 
+import type { BuildKyselyProps } from "./buildKysely.js";
 import type { PromiseFactory, Query } from "./QueryProps.js";
 
 import { d1ToKysely } from "./d1ToKysely.js";
@@ -7,9 +8,10 @@ import { resultAsyncFromPromise } from "./resultAsyncFromPromise.js";
 
 export function createQuery<DB, FIELDS extends Record<string, unknown>, OUTPUT>(
   promiseFactory: PromiseFactory<DB, FIELDS, OUTPUT | undefined>,
+  kyselyOptions?: BuildKyselyProps,
 ): Query<FIELDS, OUTPUT, "internal_server_error"> {
   return (props) => {
-    const promise = promiseFactory(d1ToKysely(props));
+    const promise = promiseFactory(d1ToKysely(props, kyselyOptions));
 
     return resultAsyncFromPromise(promise).andThen((output) =>
       output === undefined
--- src/queries/getQuery.ts
+++ src/queries/getQuery.ts
@@ -1,5 +1,6 @@
 import { errAsync, okAsync } from "neverthrow";
 
+import type { BuildKyselyProps } from "./buildKysely.js";
 import type { PromiseFactory, Query } from "./QueryProps.js";
 
 import { d1ToKysely } from "./d1ToKysely.js";
@@ -7,9 +8,10 @@ import { resultAsyncFromPromise } from "./resultAsyncFromPromise.js";
 
 export function getQuery<DB, FIELDS extends Record<string, unknown>, OUTPUT>(
   promiseFactory: PromiseFactory<DB, FIELDS, OUTPUT | undefined>,
+  kyselyOptions?: BuildKyselyProps,
 ): Query<FIELDS, OUTPUT, "internal_server_error" | "not_found"> {
   return (props) => {
-    const promise = promiseFactory(d1ToKysely(props));
+    const promise = promiseFactory(d1ToKysely(props, kyselyOptions));
 
     return resultAsyncFromPromise(promise).andThen((output) =>
       output === undefined ? errAsync("not_found" as const) : okAsync(output),
