5db7afbf8c59c4aed9305fec33a9fa2fa8ab4a46
jfreyheit <jfreyheit@gmail.com>
2025-06-09 23:03:27 +0700

    feat: serialize/unserialize dates in kysely (#99)

--- src/queries/buildKysely.ts
+++ src/queries/buildKysely.ts
@@ -1,11 +1,27 @@
 import type { LogConfig } from "kysely";
+import type { Deserializer, Serializer } from "kysely-plugin-serialize";
 
 import { D1Dialect } from "@noxharmonium/kysely-d1";
 import { CamelCasePlugin, Kysely } from "kysely";
+import { BaseSerializePlugin, dateRegex } from "kysely-plugin-serialize";
 
 export type BuildKyselyProps = {
   log?: LogConfig;
 };
+
+const serializer: Serializer = (parameter) => {
+  if (parameter instanceof Date) return parameter.toISOString();
+  return parameter;
+};
+
+const deserializer: Deserializer = (parameter) => {
+  if (typeof parameter === "string" && dateRegex.test(parameter)) {
+    return new Date(parameter);
+  }
+  return parameter;
+};
+
+const serializePlugin = new BaseSerializePlugin(serializer, deserializer, []);
 
 export function buildKysely<DB>(
   database: D1Database,
@@ -14,6 +30,6 @@ export function buildKysely<DB>(
   return new Kysely<DB>({
     ...props,
     dialect: new D1Dialect({ database }),
-    plugins: [new CamelCasePlugin()],
+    plugins: [new CamelCasePlugin(), serializePlugin],
   });
 }
